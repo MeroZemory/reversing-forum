ed4269b38b5629397a11bd376e51ebd2a05cd717
MeroZemory <merozemory@gmail.com>
2025-05-06 17:35:44 +0900

    refactor(components): flatten component directory structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import Header from "./components/organisms/Header";
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import "./styles/tailwind.css";
